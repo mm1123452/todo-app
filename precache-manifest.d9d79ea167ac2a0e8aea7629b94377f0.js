e60ab1959b5aa0aa0ca8e2ccb7199ae5c2cf1925
Mabel <macmota@hotmail.com>
2019-11-12 13:00:55 -0500

    Updates

---
+++ precache-manifest.d9d79ea167ac2a0e8aea7629b94377f0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0bee6796a07be56b5c96a5e917cba0a",
+    "url": "/todo-app/index.html"
+  },
+  {
+    "revision": "767167cff4f36870e73c",
+    "url": "/todo-app/static/css/main.fc5a0a80.chunk.css"
+  },
+  {
+    "revision": "105dfc3461421f441bb0",
+    "url": "/todo-app/static/js/2.585829d6.chunk.js"
+  },
+  {
+    "revision": "767167cff4f36870e73c",
+    "url": "/todo-app/static/js/main.bb980114.chunk.js"
+  },
+  {
+    "revision": "14566414055b74d3ad79",
+    "url": "/todo-app/static/js/runtime-main.c8041210.js"
+  }
+]);
